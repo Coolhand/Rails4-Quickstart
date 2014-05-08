5cdcc6b9c0372693dcc1882c5cba0a1485063199
Nick Harvey <Nick@dhcp7-219.lab.eng.rdu.redhat.com>
2014-05-08 17:17:27 -0400

    rails 4 creation and configuration of Openshift quickstart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4quickstart::Application.config.secret_key_base = '062c60a54f02c04e79eaa8a1504eddb642204a37e7ae49f2af2510f40024f5f50a49a44b3d3f10d944117c54472a35e8bfe234e20336c7325403e8a2e286f282'
